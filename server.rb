7ee5701d2a3cfcfbd164e76e1bd2b602d3cc3116
Florent Delannoy <florent.delannoy@gmail.com>
2011-04-30 23:16:34 +0200

    Implemented realistic rumors, code cleanup

--- server.rb
+++ server.rb
@@ -30,48 +30,84 @@ helpers do
 		return result
 	end
 	def person id
-		dbquery('select name from persons where rowid=?', id).first.first
+		dbquery('SELECT name
+			 FROM persons
+			 WHERE rowid=?', id).first.first
 	end
 	def option id
-		dbquery('select opt from options where rowid=?', id).first.first
+		dbquery('SELECT opt
+			 FROM options
+			 WHERE rowid=?', id).first.first
 	end
 	def random_person
-		dbquery('select rowid from persons where rowid>=(abs(random()) % (select 1+count(name) from persons)) limit 1').first.first
+		dbquery('SELECT rowid
+			 FROM persons
+			 WHERE rowid>=(abs(random()) % (SELECT 1+COUNT(name) FROM persons))
+			 LIMIT 1').first.first
 	end
 	def random_option
-		dbquery('select rowid from options where rowid>=(abs(random()) % (select 1+count(opt) from options)) limit 1').first.first
+		dbquery('SELECT rowid
+			 FROM options
+			 WHERE rowid>=(abs(random()) % (SELECT 1+COUNT(opt) FROM options))
+			 LIMIT 1').first.first
 	end
 	def get_vote_for(person, option)
 		yes, no = 0, 0
-		no = dbquery('select count(*) from votes where person=? and option=? and value=0', [person, option]).first.first
-		yes = dbquery('select count(*) from votes where person=? and option=? and value=1', [person, option]).first.first
+		no = dbquery('SELECT COUNT(*)
+			      FROM votes
+			      WHERE person=? AND option=? AND value=0', [person, option]).first.first
+		yes = dbquery('SELECT COUNT(*)
+			       FROM votes
+			       WHERE person=? AND option=? AND value=1', [person, option]).first.first
 		return [yes, no]
 	end
 	def add_vote_for(person, option, value)
-		dbquery 'insert into votes(person, option, value) values(?, ?, ?)', [person, option, value]
-	end
-	def top10_votes
-		top10 = []
-		dbquery('SELECT person, name, option, opt, count(value) AS total FROM votes, persons, options WHERE person = persons.rowid AND option=options.rowid GROUP BY person, option ORDER BY total DESC LIMIT 10').each do |row|
-				person_id, option_id = row[0], row[2]
-				top10 << { :person => row[1],
-					   :option => row[3],
-					   :votes => (get_vote_for person_id, option_id)}
-		end
-		return top10
+		dbquery 'INSERT INTO votes(person, option, value) VALUES(?, ?, ?)', [person, option, value]
 	end
 	def top10_votes_true
+		votes = {}
+		dbquery('SELECT name, opt, value
+			 FROM votes, persons, options
+			 WHERE person = persons.rowid AND option=options.rowid
+			 ORDER BY person').each do |row|
+			# We are creating a hash who maps a rumor to its credibility.
+			# The rumor is (an array) defined by the person and the option:
+			hashkey = [row[0], row[1]]
+			# And its credibility is a two-elements array [yes, no]
+			votes[hashkey] ||= [0,0]
+			votes[hashkey][row[2].to_i] += 1
+		end
+		weighed_votes = votes.dup
+		weighed_votes.merge!(votes){ |k,v| # merge() is used as sort of map() over a hash
+			# Behold the awe-inspiring ranking algorithm!
+			(v[1]**1.8) / (v[0]+1)
+		}
 		top10 = []
-		dbquery('SELECT person, name, option, opt, count(value) AS total FROM votes, persons, options WHERE person = persons.rowid AND option=options.rowid GROUP BY person, option ORDER BY total DESC LIMIT 10').each do |row|
-				person_id, option_id = row[0], row[2]
-				top10 << { :person => row[1],
-					   :option => row[3],
-					   :votes => (get_vote_for person_id, option_id)}
+		until top10.length == 10 or weighed_votes.empty?
+			max = weighed_votes.index(weighed_votes.values.max)
+			top10 << { :person => max[0],
+				   :option => max[1],
+				   :votes => votes[max].reverse
+				   # reverse: because JS expects [yes,no] whilst we populated the array with [0] as noes and [1] as yeses
+			}
+			weighed_votes.delete max
 		end
 		return top10
 	end
-	def top10_comments
-
+	def top10_most_votes
+		top10 = []
+		dbquery('SELECT person, name, option, opt, COUNT(value) AS total
+			 FROM votes, persons, options
+			 WHERE person = persons.rowid AND option=options.rowid
+			 GROUP BY person, option
+			 ORDER BY total DESC
+			 LIMIT 10').each do |row|
+			person_id, option_id = row[0], row[2]
+			top10 << { :person => row[1],
+				   :option => row[3],
+				   :votes => (get_vote_for person_id, option_id)}
+		end
+		return top10
 	end
 end
 
@@ -104,7 +140,7 @@ end
 
 get '/reorg/topvoted' do
 	content_type :json
-	JSON	top10_votes
+	JSON	top10_most_votes
 end
 
 get '/reorg/topvotedtrue' do
